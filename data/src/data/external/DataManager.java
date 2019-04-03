d9e267253eed7989f31715d9cde43fc1540e698c
Ryan Culhane <rpc21@duke.edu>
2019-04-03 17:10:08 -0400

    Working on #56.  Able to create a new file and save it to a specified directory.  Created classes in the Data module for testing and sandbox purposes, will refactor and delete when functionality is set

---
+++ data/src/data/external/DataManager.java
@@ -0,0 +1,36 @@
+package data.external;
+
+public class DataManager implements ExternalData{
+
+
+
+    @Override
+    public Object loadGameDisplay(String gameName) {
+        return null;
+    }
+
+    @Override
+    public void saveGameDisplay(String gameName) {
+
+    }
+
+    @Override
+    public Object loadGame(String gameName) {
+        return null;
+    }
+
+    @Override
+    public Object continueGame(String gameName) {
+        return null;
+    }
+
+    @Override
+    public void saveGame(String gameName) {
+
+    }
+
+    @Override
+    public void createFolder(String folderName) {
+
+    }
+}
---
+++ data/src/data/internal/DataSandbox.java
@@ -0,0 +1,31 @@
+package data.internal;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DataSandbox {
+
+    public static void main(String[] args) {
+        FileWriter fileWriter = null;
+        System.out.println("user.dir");
+        try {
+            fileWriter =
+                    new FileWriter(new File("data" + File.separator + "resources" + File.separator, "myTestFile"));
+            fileWriter.write("Here is my string, I hope you like it");
+        } catch (IOException exception){
+            System.out.println("In catch block");
+            exception.printStackTrace();
+        } finally {
+            System.out.println("In finally block");
+            try {
+                if (fileWriter != null){
+                    fileWriter.close();
+                }
+            } catch (IOException e){
+                System.out.println("Couldn't close file");
+            }
+        }
+
+    }
+}
