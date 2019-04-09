c35df67054b74dc1b7f4555d42274829c848c0e5
Dima Fayyad <dkf8D@duke.edu>
2019-04-08 21:22:52 -0400

    Implemented the Four Collision Conditions (for each direction of up,down,left,right) closes #95

--- authoring/src/events/TopCollisionEvent.java
+++ authoring/src/events/TopCollisionEvent.java
@@ -1,4 +1,26 @@
 package events;
 
-public class TopCollisionEvent {
+import conditions.CollisionCondition;
+import conditions.RightUpCollisionCondition;
+import engine.external.component.CollidedComponent;
+import engine.external.component.YPositionComponent;
+
+public class TopCollisionEvent extends Event {
+
+    private String myCollisionWithEntity;
+
+    public TopCollisionEvent(String name, String collideWithEntity){
+        super(name);
+        myCollisionWithEntity = collideWithEntity;
+
+        makeTopCollisionCondition();
+    }
+    /**
+     * Adds a condition to the Event that verifies entity has a collidedComponent containing the correct entity collided with
+     * Adds a condition to the Event that verifies the collision is on top of entity
+     */
+    private void makeTopCollisionCondition(){
+        CollisionCondition containsCollidedComponentCondition = new RightUpCollisionCondition(CollidedComponent.class, myCollisionWithEntity, YPositionComponent.class);
+        addConditions(containsCollidedComponentCondition);
+    }
 }
