c3e099a0c42d95a11d7c704d563b977594bd96fc
Feroze Mohideen <fm87@duke.edu>
2019-04-10 21:32:58 -0400

    added file for jar file to run

---
+++ runner/src/runner/internal/DataTestRunner2.java
@@ -0,0 +1,9 @@
+package runner.internal;
+
+public class DataTestRunner2 {
+    public static void main(String[] args){
+        System.out.println("DataTestRunner");
+        RunnerTester2.main(args);
+        //GameRunner runner = new GameRunner("game1");
+    }
+}
