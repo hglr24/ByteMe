57c1d933fe35f8c97a9c3fe0c9c9d860012e9e39
Anna Darwish <ard51@duke.edu>
2019-04-28 00:47:03 -0400

    Added functionality to user manager to navigate between pages once the user has logged in

--- launcher/src/manager/UserManager.java
+++ launcher/src/manager/UserManager.java
@@ -9,25 +9,35 @@ import runner.external.Game;
 import ui.main.MainGUI;
     class UserManager{
     private CreateNewGamePage myNewGamePage;
+
+    private SwitchToUserPage switchToUserPage = new SwitchToUserPage() {
+        @Override
+        public void switchUserPage(String userName) {
+            goToGameCenter();
+        }
+    };
     private String myUserName;
+    private Stage myStage;
     UserManager(String userName){
         myUserName = userName;
-    }
-    void render(Stage myStage){
-        makePages();
 
+    }
+    void render(Stage stage){
+        myStage = stage;
+        makePages();
     }
 
     private void makePages(){
 
     }
+
 //    private void goToAuthoring(GameCenterData myData){
 //        MainGUI myGUI = new MainGUI(new Game(), myData);
 //        myGUI.launch();
 //    }
 //
-//    private void goToGameCenter(){
-//        CenterView myCenter = new CenterView();
-//        myStage.setScene(myCenter.getScene());
-//    }
+    private void goToGameCenter(){
+        CenterView myCenter = new CenterView();
+        myStage.setScene(myCenter.getScene());
+    }
 }
