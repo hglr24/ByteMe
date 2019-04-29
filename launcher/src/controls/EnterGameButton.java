ed407cc2faef577b5a5cc8d32e1659b6fdbaa72e
Anna Darwish <ard51@duke.edu>
2019-04-28 23:44:17 -0400

    added in options to either create a new game or reload a new game to get into authoring

--- launcher/src/controls/EnterGameButton.java
+++ launcher/src/controls/EnterGameButton.java
@@ -4,7 +4,7 @@ package controls;
 import data.external.DataManager;
 import manager.SwitchToUserPage;
 import popup.ErrorPopUp;
-import popup.LoginErrorPopUp;
+
 public class EnterGameButton extends SceneSwitchButton {
     private static final String STYLE = "default_launcher.css";
     private CredentialValidator userNameAccessor;
@@ -40,7 +40,7 @@ public class EnterGameButton extends SceneSwitchButton {
     }
 
     private void displayInvalidLogin(){
-        ErrorPopUp invalidLogin = new LoginErrorPopUp(ERROR);
+        ErrorPopUp invalidLogin = new ErrorPopUp(ERROR);
         invalidLogin.display();
     }
 
