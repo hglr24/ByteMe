caf85118bf72badf9f62b6d64e5c759ac5a1e750
Anna Darwish <ard51@duke.edu>
2019-05-05 17:04:00 -0400

    refactored event factory and authoring event for coding masterpiece
    Former-commit-id: 3bada9dd6c0b12d912f3116faa5b3570f9304037

--- authoring/src/events/EventFactory.java
+++ authoring/src/events/EventFactory.java
@@ -2,27 +2,42 @@ package events;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.*;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import java.util.*;
+import java.util.List;
+import java.util.Collections;
+import java.util.ResourceBundle;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Set;
 
 /**
- * This is essentially a Utilities class that EventPane uses in order to display particular options and properties associated
+ * This is a Utilities class that EventPane and AuthoringEvents use in order to display particular options and properties associated
  * with engine.external.events and engine.external.actions. This helps with reflection in creating engine.external.events, as different engine.external.events need to provide different
- * controls to the user to input information necessary to instantiate different engine.external.events and engine.external.actions
+ * controls to the user to input information necessary to instantiate different engine.external.events and engine.external.actions. One
+ * issue I struggled a lot with was ensuring there wasn't duplicated code and that the controls displayed to the user would be able
+ * to toggle according to their input. Since conditions and actions were supposed to be the most flexible point of our project, I created
+ * this class to generalize the problem as much as possible so different areas of the project could reuse the methods to display whatever
+ * would be needed for the user to be presented with all the options at their disposal.
  * @author Anna Darwish
  */
 public class EventFactory {
     private static final String ASSOCIATED_OPTIONS_DELIMITER = "::";
     private static final String KEYCODE_DELIMITER = ".";
+    private static final String STYLE = "default.css";
+    private static final String STYLE_EVENTS = "event-options";
     private static List<String> getIndependentOptionsListing(ResourceBundle associatedOptionsResource){
         List<String> independentOptions = new ArrayList<>(associatedOptionsResource.keySet());
         Collections.sort(independentOptions);
         return independentOptions;
     }
-
+    /**
+     * This method returns a map of associated options with a particular key value from a properties file. This is useful for
+     * toggling drop down menu's whose values change depending on the value currently selected in the first drop-down menu.
+     */
     private static Map<String, ObservableList<String>> createAssociatedOptions(String bundleName){
         Map<String, ObservableList<String>> associatedOptions = new HashMap<>();
         ResourceBundle associatedOptionsResource = ResourceBundle.getBundle(bundleName);
@@ -35,7 +50,11 @@ public class EventFactory {
 
         return associatedOptions;
     }
-
+    /**
+     * This method handles setting up a keycode choice-box from a resource file that has indexed keycodes. Since there are
+     * multiple areas that a user may toggle an event's associated key codes, it was useful to keep this in the utilities class
+     * as it is then easier to ensure any key code changes we would ever need to make
+     */
     public static void setUpKeyCode(ResourceBundle myKeyCodes, ChoiceBox<String> myKeyCodesListing){
         Set<String> keyCodes = myKeyCodes.keySet();
         List<String> keyCodesList = new ArrayList<>(keyCodes);
@@ -47,9 +66,14 @@ public class EventFactory {
         myKeyCodesListing.setItems(FXCollections.observableList(removedIndex));
         myKeyCodesListing.setOnAction(actionEvent -> myKeyCodesListing.setAccessibleText(myKeyCodesListing.getValue()));
     }
-
+    /**
+     * This method handles setting up event component display as this prompt appears in multiple settings. It also binds
+     * the values of the controls to the StringProperty values that are passed to it
+     * @see ValueFieldProperty
+     * @return HBox that prompts the user to enter information about the component whose value they are setting a conditional on
+     */
     public static HBox createEventComponentOptions(String prompt, String resourceName, StringProperty componentName,
-                                               StringProperty modifierOperator, StringProperty triggerValue){
+                                                   StringProperty modifierOperator, StringProperty triggerValue){
         HBox eventComponentOptions = new HBox();
         Label userPrompt = EventFactory.createLabel(prompt);
         eventComponentOptions.getChildren().add(userPrompt);
@@ -61,13 +85,20 @@ public class EventFactory {
         triggerValue.bindBidirectional(myTriggerControl.textProperty());
         createDependencyForValueField(componentChoiceBox,myTriggerControl);
         eventComponentOptions.getChildren().add(myTriggerControl);
-        eventComponentOptions.getStyleClass().add("event-options");
+        eventComponentOptions.getStyleClass().add(STYLE_EVENTS);
         return eventComponentOptions;
 
     }
-
+    /**
+     * This method binds the value of an independent choice box to a dependent one so that the latter's options change
+     * depending on the independent's current value.
+     * @param actionOperatorOptions maps a possible independent value to the associated options that should appear in the dependent
+     *                              choice-box
+     * @see ValueFieldProperty
+     * @return HBox that prompts the user to enter information about the component whose value they are setting a conditional on
+     */
     public static ChoiceBox<String> setUpPairedChoiceBoxes(Map<String,ObservableList<String>> actionOperatorOptions,
-                                                       StringProperty controller, StringProperty dependent,HBox parent){
+                                                           StringProperty controller, StringProperty dependent,HBox parent){
         List<String> componentOptions = new ArrayList<>(actionOperatorOptions.keySet());
         Collections.sort(componentOptions);
         ChoiceBox<String> componentChoiceBox = setUpBoundChoiceBox(componentOptions,controller,parent);
@@ -77,7 +108,7 @@ public class EventFactory {
     }
 
     private static void createDependencyBetweenChoiceBoxes(ChoiceBox<String> controller, ChoiceBox<String> controllee,
-                                                      Map<String,ObservableList<String>> conditionOperatorOptions){
+                                                           Map<String,ObservableList<String>> conditionOperatorOptions){
         controller.getSelectionModel().selectedItemProperty().addListener((observableEvent, oldComponent, newComponent) -> {
             controllee.setItems(FXCollections.observableList(conditionOperatorOptions.get(newComponent)));
             if (conditionOperatorOptions.get(newComponent).size() == 1){
@@ -101,10 +132,12 @@ public class EventFactory {
         });
     }
 
-
+    /**
+     * This method returns a label to be displayed in an event-specific prompt box
+     */
     public static Label createLabel(String labelText){
         Label myLabel = new Label(labelText);
-        myLabel.getStylesheets().add("default.css");
+        myLabel.getStylesheets().add(STYLE);
         return myLabel;
     }
 
--- authoring/src/ui/AuthoringEvent.java
+++ authoring/src/ui/AuthoringEvent.java
@@ -10,35 +10,48 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import ui.manager.RefreshEvents;
-
-import java.util.*;
-
+import java.util.Map;
+/**
+ * This superclass helps to handle a lot of the similarities of display and saving among the differing authoring events.
+ * Because they all significantly differ in the information they may require and the way they are saved, a hierarchy
+ * structure seemed to fit best for handling the different forms an event a user may wish to create. It is important to note
+ * that this hierarchy does not just duplicate the hierarchy of our engine's event hierarchy, nor does it require creating a
+ * new subclass for every type of event created. Instead, this allows the event prompts to receive exactly the information they
+ * need but still help in removing duplicate code by handling aspects common to all events, such as an action.
+ * @author Anna Darwish
+ */
 public abstract class AuthoringEvent extends Stage {
     private static final String USER_ACTION_PROMPT = "Action:";
     private static final String ACTION_RESOURCE = "actions_display";
     private static final String CANCEL = "Cancel";
     private static final String CHECKPOINT = "Click to Insert CheckPoint";
     private static final String DEFAULT_STYLESHEET = "default.css";
+    private static final String BUTTONS_DISPLAY = "buttons-bar";
+    private static final String BUILDER_STYLE = "event-builder";
 
     private StringProperty componentName = new SimpleStringProperty(); //Name of the component for the conditional
     private StringProperty modifierOperator = new SimpleStringProperty(); //type of modifier such as set or scale
     private StringProperty triggerValue = new SimpleStringProperty();  //value associated with action
     private BooleanProperty checkPoint = new SimpleBooleanProperty();
 
+    /**
+     * The subclasses differ primarily in regards to what conditions options they offer to users, so this vbox is meant
+     * to allow the user to specific what they wish to prompt to the user for the conditions
+     */
     abstract VBox generateEventOptions();
-
+    /**
+     * These save components are necessary for any event to add in the event generated when the user saves the event they
+     * created from the prompt box, and then to refresh the event display pane to show the changes the user just saved. Since
+     * saving happens differently because of the different conditions involved in the subclasses, they handle using these
+     * differently
+     */
     public abstract void addSaveComponents(RefreshEvents myRefresher, ObservableList<Event> myEntityEvents);
 
     void saveEvent(Event createdEvent, RefreshEvents myRefresher, ObservableList<Event> myEntityEvents){
@@ -46,12 +59,16 @@ public abstract class AuthoringEvent extends Stage {
         myRefresher.refreshEventDisplay(createdEvent);
         this.close();
     }
-
+    /**
+     * This method is invoked when a user chooses add a new event and displays the pop-up associated with the event they
+     * selected. It invokes the subclass's generateEventOptions method, which handles displaying the conditions for that
+     * particular event
+     */
     public void render(){
         VBox eventOptions = generateEventOptions();
         Scene myScene = new Scene(eventOptions);
         myScene.getStylesheets().add(DEFAULT_STYLESHEET);
-        eventOptions.getStyleClass().add("event-builder");
+        eventOptions.getStyleClass().add(BUILDER_STYLE);
         this.setScene(myScene);
         this.sizeToScene();
         this.show();
@@ -61,10 +78,7 @@ public abstract class AuthoringEvent extends Stage {
         VBox actionOptions = new VBox();
         HBox actions = createEventComponentOptions(USER_ACTION_PROMPT,ACTION_RESOURCE,componentName,modifierOperator,triggerValue);
         ToggleButton myButton = new ToggleButton(CHECKPOINT);
-        myButton.setOnMouseClicked(mouseEvent -> {
-            if (myButton.isSelected())
-                myButton.setTextFill(Color.AQUAMARINE);
-        });
+        myButton.getStyleClass().add(BUILDER_STYLE);
         checkPoint.bindBidirectional(myButton.selectedProperty());
         actionOptions.getChildren().add(actions);
         actionOptions.getChildren().add(myButton);
@@ -76,8 +90,9 @@ public abstract class AuthoringEvent extends Stage {
         try {
             Action createdAction = myBuilder.createGeneralAction(componentName.getValue(), modifierOperator.getValue(), triggerValue.getValue());
             buildingEvent.addActions(createdAction);
-            if (checkPoint.getValue())
+            if (checkPoint.getValue()) {
                 buildingEvent.addActions(new ProgressionAction(true));
+            }
         }
         catch (UIException e){
             e.displayUIException();
@@ -85,19 +100,19 @@ public abstract class AuthoringEvent extends Stage {
     }
 
     HBox createEventComponentOptions(String prompt, String resourceName, StringProperty componentName,
-                                               StringProperty modifierOperator, StringProperty triggerValue){
+                                     StringProperty modifierOperator, StringProperty triggerValue){
         return EventFactory.createEventComponentOptions(prompt,resourceName,componentName,modifierOperator,triggerValue);
 
     }
 
-    ChoiceBox<String> setUpPairedChoiceBoxes(Map<String,ObservableList<String>> actionOperatorOptions,
-                                                     StringProperty controller, StringProperty dependent,HBox parent){
-        return EventFactory.setUpPairedChoiceBoxes(actionOperatorOptions,controller,dependent,parent);
+    void setUpPairedChoiceBoxes(Map<String,ObservableList<String>> actionOperatorOptions,
+                                StringProperty controller, StringProperty dependent,HBox parent){
+        EventFactory.setUpPairedChoiceBoxes(actionOperatorOptions,controller,dependent,parent);
     }
 
     HBox createToolBar(Button saveButton){
         HBox toolBar = new HBox();
-        toolBar.getStyleClass().add("buttons-bar");
+        toolBar.getStyleClass().add(BUTTONS_DISPLAY);
         Button cancelButton = new Button(CANCEL);
         cancelButton.setOnMouseClicked(mouseEvent -> closeWindow());
         toolBar.getChildren().add(saveButton);
@@ -110,3 +125,4 @@ public abstract class AuthoringEvent extends Stage {
     }
 
 }
+
